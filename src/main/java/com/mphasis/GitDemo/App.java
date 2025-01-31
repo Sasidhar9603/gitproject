f60571291206e69dcaca7c0b90d693ac5617c58a
Sasidhar <korivisasidhar00@gmail.com>
2025-01-31 12:52:07 +0530

    second

---
+++ src/main/java/com/mphasis/GitDemo/App.java
@@ -0,0 +1,16 @@
+package com.mphasis.GitDemo;
+public class App{
+	public void addData(String data) {
+		System.out.print("Data"+data);
+		if(data.equals("Sasidhar")) {
+			System.out.println("Welcome");
+		}
+	}
+	public void printData(String data) {
+		System.out.println("Data : "+data);
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+//jgaijgo
+}
